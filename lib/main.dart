d73fb60bfcdb8ce4bc2e4e4b88b5676106c38fd4
cristianfchq <cristian.fchq@gmail.com>
2019-06-26 01:32:11 -0400

    creacion de los tabs que estan en la variable Tabs Controller

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,56 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_tabs/tabs/inicio_tabs.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-
+    //creamos la variable TabController donde estaran todos nuestro tabs de navegacion
+    final TabController = DefaultTabController(
+      length: 4, //numero de los iconos o tabs de la barra
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            'Navegacion por Tabs',
+          ),
+          bottom: TabBar(
+            indicatorColor: Colors.red,
+            tabs: <Widget>[
+              Tab(
+                icon: Icon(Icons.home),
+                text: "Inicio",
+              ),
+              Tab(
+                icon: Icon(Icons.contacts),
+                text: "Registro",
+              ),
+              Tab(
+                icon: Icon(Icons.place),
+                text: "Lugares",
+              ),
+              Tab(
+                icon: Icon(Icons.assignment_late),
+                text: "Acerca de ...",
+              ),
+            ],
+          ),
+        ),
+        //
+        body: TabBarView(
+          children: <Widget>[
+            inicioTabs(),
+          ],
+        ),
+      ),
+    );
+    //
+    return MaterialApp(
+      title: 'Tabs con Flutter',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: TabController,
     );
   }
 }
---
+++ lib/tabs/inicio_tabs.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class inicioTabs extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
